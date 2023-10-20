6194a34a303a156bdcb2963194822fee13dfda95
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-20 17:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+var form =document.getElementById("form1");
 
+	var firstName= form.fname.value;
+	var lastName= form.lname.value;
+
+	 var outputMessage =  firstName +" "+  lastName;
+
+	alert(outputMessage);
 }
